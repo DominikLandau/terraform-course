4a8cb73722cb606f2a8a21f4c4d13c08b2365de8
Ubuntu <dominik@terra.2z3vxojurm5e5aeaqds5cy2a0d.frax.internal.cloudapp.net>
2026-02-20 12:51:30 +0000

    removed

--- examples/appgateway/provider.tf
+++ examples/appgateway/provider.tf
@@ -16,8 +16,4 @@ terraform {
 provider "azurerm" {
   features {}
 
-  tenant_id       = "e9d9e2fa-f59f-4299-ac5d-a4a253abfbb8"
-  client_secret   = "WRe8Q~ePOGW1MmCIOjPHGbITTke5xOZYgnbJ7c~x"
-  subscription_id = "b839dba6-c92d-4975-8ea0-ec65addc6677"
-  client_id       = "aeade839-4b81-488d-8d60-de50d12069ab"
 }
--- examples/networks/main.tf
+++ examples/networks/main.tf
@@ -1,4 +1,15 @@
+locals {
+  networks = {
+    prod = "/sub/erfeerji"
+    test = "/sfsfs"
+  }
+}
+
 data "azurerm_virtual_network" "prod" {
   resource_group_name = "kurs1"
   name     = "vnet1"
+}
+
+module "test" {
+  network = data.azurerm_virtual_network.${local.env}
 }
